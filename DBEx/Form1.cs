29d2823fb41c2080612532a21411237a7e77d5dc
vomo3732 <42690305+vomo3732@users.noreply.github.com>
2021-03-22 17:43:24 +0900

    Add files via upload

---
+++ DBEx/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDBManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            string str = tbInput.Text; 
+            
+            dbGrid.Columns.Add(str, str);
+            dbGrid.Rows.Add( );
+        }
+
+        private void btnValue_Click(object sender, EventArgs e)
+        {
+            string str = tbValue.Text;
+            dbGrid.Rows[1].Cells[2].Value = str;
+        }
+    }
+}
